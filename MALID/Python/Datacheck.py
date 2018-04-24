7c419e4b36d00a8e6ca2ebdbe52bbcae186250e5
pjh6818 <a6846818@naver.com>
2018-04-24 22:21:26 +0900

    운동종류 수정

--- MALID/Python/Datacheck.py
+++ MALID/Python/Datacheck.py
@@ -10,40 +10,25 @@ Label=xy[:, [-1]]
 stop=0
 walk=0
 run=0
+walk_hand=0
+run_hand=0
 bike=0
-stair=0
-runningmachine=0
-pushup=0
-situp=0
-squat=0
 etc=0
-
 #for x in xyz_data:
     #print(x)
-for y in Label:
+for idx,y in enumerate(Label):
     if y[0]==0:stop+=1
     elif y[0]==1:walk+=1
     elif y[0]==2:run+=1
-    elif y[0]==3:bike+=1
-    elif y[0]==4:stair+=1
-    elif y[0]==5:runningmachine+=1
-    elif y[0]==6:pushup+=1
-    elif y[0]==7:situp+=1
-    elif y[0]==8:squat+=1
-    else : etc+=1
+    elif y[0]==3:walk_hand+=1
+    elif y[0]==4:run_hand+=1
+    elif y[0]==5:bike+=1
+    else:etc+=1
 print(sys.argv[1],"Data Status")
 print("Stop : ", stop)
 print("Walk : ", walk)
 print("Run : ", run)
+print("Walk_hand : ", walk_hand)
+print("Run_hand : ", run_hand)
 print("Bike : ", bike)
-print("Stair : ", stair)
-print("RunningMachine : ", runningmachine)
-print("Pushup : ", pushup)
-print("Situp : ", situp)
-print("Squat : ", squat)
 print("Etc : ", etc)
-
-
-
-
-
