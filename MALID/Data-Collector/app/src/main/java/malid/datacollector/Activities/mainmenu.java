ee63845cbc8f8f81b16866e3822354ffeba711f1
kty9418 <31645201+kty9418@users.noreply.github.com>
2018-05-10 20:31:52 +0900

    로그인 기능추가
    nodejs 서버에서 DB연동하여 로그인하는 기능 추가
    (예제 서버파일 login.js
    DB : MALID
    TABLE 명 : login)

--- MALID/Data-Collector/app/src/main/java/malid/datacollector/Activities/mainmenu.java
+++ MALID/Data-Collector/app/src/main/java/malid/datacollector/Activities/mainmenu.java
@@ -1,17 +1,34 @@
 package malid.datacollector.Activities;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import malid.datacollector.R;
 
@@ -20,20 +37,30 @@ import malid.datacollector.R;
  */
 public class mainmenu extends AppCompatActivity{
 
-    public String ID = null;    // 사용자 ID
+    public String ID = "";    // 사용자 ID
+    public String Passwd = "";    // 사용자 passwd
+    View dialogView;
+    EditText temp1;
+    CheckBox checkBox = null;
+    boolean login = false;
+    SharedPreferences sf;
+    SharedPreferences.Editor editor;
+    String login_url = "http://13.125.101.194:3000/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
+        init_login();
+
 
         Button btn1 = (Button)findViewById(R.id.recordgo); //button이라는 id를 가진 버튼뷰를 가져와서
         Button btn2 = (Button)findViewById(R.id.historygo);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //클릭이 발생했을때.
-                if(ID == null){
-                    Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_LONG).show();
+                if(login == false){
+                    Toast.makeText(getApplicationContext(), "로그인 해주세요", Toast.LENGTH_LONG).show();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "제작 : MALID", Toast.LENGTH_LONG).show();
@@ -49,8 +76,8 @@ public class mainmenu extends AppCompatActivity{
         btn2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                if(ID == null){
-                    Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_LONG).show();
+                if(login == false){
+                    Toast.makeText(getApplicationContext(), "로그인 해주세요", Toast.LENGTH_LONG).show();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "제작 : MALID", Toast.LENGTH_LONG).show();
@@ -58,6 +85,18 @@ public class mainmenu extends AppCompatActivity{
                 }
             }
         });
+    }
+
+    void init_login(){
+        sf = getSharedPreferences("data", MODE_PRIVATE);
+        editor = sf.edit();
+        ID = sf.getString("ID", "");
+        Passwd = sf.getString("Passwd","");
+        Log.v("test", "init_login ----------");
+        Log.v("test", "ID : "+ID+" Passwd : "+Passwd);
+        if(!ID.isEmpty() && !Passwd.isEmpty()) {
+            new JSONTask().execute(login_url);
+        }
     }
 
     @Override
@@ -72,34 +111,52 @@ public class mainmenu extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch(item.getItemId()){
-            case R.id.input_ID:
-                AlertDialog.Builder ad = new AlertDialog.Builder(mainmenu.this);
+            case R.id.login:
+                if(login == false){
+                    dialogView = (View) View.inflate(mainmenu.this, R.layout.dialog_login, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(mainmenu.this);
+                    //dlg.setTitle("Login");
+                    dlg.setView(dialogView);
+                    dlg.setPositiveButton("sign in", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            temp1 = dialogView.findViewById(R.id.ID_text);
+                            ID = temp1.getText().toString();
+                            temp1 = dialogView.findViewById(R.id.Passwd_text);
+                            Passwd = temp1.getText().toString();
+                            checkBox = dialogView.findViewById(R.id.Auto_login);
 
-                ad.setTitle("ID입력");       // 제목 설정
-                ad.setMessage("사용할 ID를 입력하세요");   // 내용 설정
+                            if(ID.isEmpty() || Passwd.isEmpty()) {
+                                Toast.makeText(getApplicationContext(), "ID와 Password를 입력해주세요", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                new JSONTask().execute(login_url);
+                            }
 
-// EditText 삽입하기
-                final EditText et = new EditText(mainmenu.this);
-                ad.setView(et);
+                        }
+                    });
 
-// 확인 버튼 설정
-                ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ID = et.getText().toString();
-                        dialog.dismiss();     //닫기
-                    }
-                });
+                    dlg.setNegativeButton("cancel", null);
+                    dlg.show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "이미 로그인되어 있습니다.", Toast.LENGTH_LONG).show();
+                }
+                return true;
+            case R.id.logout:
+                if(login) {
+                    login = false;
+                    editor.remove("ID");
+                    editor.remove("Passwd");
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "로그인되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+                }
+                return true;
+            case R.id.join:
 
-// 취소 버튼 설정
-                ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();     //닫기
-                    }
-                });
-
-                ad.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -113,5 +170,88 @@ public class mainmenu extends AppCompatActivity{
         }
     };*/
 
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("ID", ID);
+                jsonObject.accumulate("PASSWD", Passwd);
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Cache-Control", "no-cache");
+                    con.setRequestProperty("Content-Type", "application/json");
+                    con.setRequestProperty("Accept", "text/html");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+                    con.connect();
+
+                    OutputStream outStream = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();
+
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            if(result.equals("로그인 되었습니다.")){
+                login = true;
+                if(checkBox != null) {
+                    if (checkBox.isChecked()) {
+                        Log.v("test", "ID & Passwd save");
+                        editor.putString("ID", ID);
+                        editor.putString("Passwd", Passwd);
+                        editor.commit();
+                    }
+                }
+            }
+        }
+    }
 
 }
