2b534192ae9e8e1d30aa889e37b64562e04b379a
song <thdals778@naver.com>
2018-05-15 03:13:22 +0900

    데이터 시각화
    데이터 시각화

--- MALID/Python/graph.py
+++ MALID/Python/graph.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.interactive(False)
 plt.style.use('ggplot')
-plt.title('Accelerometer value histogram')
-plt.xlabel('Accelerometer value')
-plt.ylabel('Frequency')
+plt.xlabel('Index')
+plt.ylabel('Magnitude')
 xy=np.loadtxt('4.txt', delimiter=',', dtype=np.float32)
-
-HR=xy[:, [0]]
 xyz_data=xy[:, 1:-1]
 Label=xy[:, [-1]]
-label = 0;
-idx = 0;
 label = np.unique(Label)
 for i in label:
-    plt.hist(xyz_data[np.where(Label == i)[0]])
-
+    plt.title('Unsorted Accelerometer value histogram : '+str(int(i)))
+    xyz=xyz_data[(np.where(Label == i))[0][5]]
+    plt.scatter(np.where(xyz), xyz)
+    fig = plt.gcf()
+    plt.show()
+    fig.savefig('Unsort_'+str(i)+'.png')
+    plt.title('Sorted Accelerometer value histogram : '+str(int(i)))
+    sort_xyz = np.sort(xyz_data[(np.where(Label == i))[0][5]])
+    plt.scatter(np.where(xyz), sort_xyz)
+    fig = plt.gcf()
+    plt.show()
+    fig.savefig('Sort_'+str(i)+'.png')
